7efbf46051920e8a28421b516860add87733e3b3
ckyap2022 <95077467+ckyap2022@users.noreply.github.com>
2021-11-27 15:32:41 +0800

    Add schema

--- Section2/query.sql
+++ Section2/query.sql
@@ -1,8 +1,8 @@
 
 1) I want to know the list of our customers and their spending.
 
-select b.name, sum(sellingprice) from transaction a
-inner join customer b on (b.customer_id=a.customer_id)
+select b.name, sum(sellingprice) from public.transaction a
+inner join public.customer b on (b.customer_id=a.customer_id)
 where a.voided !='Y'
 group by b.name
 order by 2 desc
@@ -10,9 +10,9 @@ order by 2 desc
 2) I want to find out the top 3 car manufacturers that customers bought by sales (quantity) and the sales number for it in the current month.
 
 select * from (
-select c.name, a.order_no, count(a.order_no) from transaction a
-inner join car b on (b.car_id=a.car_id)
-inner join car_manufacturer c on (c.car_manufacturer_id=b.car_manufacturer_id)
+select c.name, a.order_no, count(a.order_no) from public.transaction a
+inner join public.car b on (b.car_id=a.car_id)
+inner join public.car_manufacturer c on (c.car_manufacturer_id=b.car_manufacturer_id)
 where a.voided !='Y' and transact_dt>=date_trunc('month',CURRENT_DATE)
 group by c.name, a.order_no) z
 order by 3 desc
